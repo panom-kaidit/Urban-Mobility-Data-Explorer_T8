e1ee52c227fd37271ec9281ac92b20b7f7b2541b
Panom M <p.makuei@alustudent.com>
2026-06-16 16:42:50 +0300

    Add CORS middleware and initialize database on startup

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,29 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from backend.routes import trips, locations, suspicious_records
+from backend.config.database import init_db
 
 app = FastAPI(
     title="Urban Mobility Data Explorer",
     version="1.0.0"
 )
 
+app.add_middleware(
+    # we allow cors from any origin for simplicity and it will be  changed in the future when backend is on a different domain than frontend. --- IGNORE ---
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.on_event("startup")
+def on_startup():
+    init_db()
+
 app.include_router(trips.router)
 app.include_router(locations.router)
 app.include_router(suspicious_records.router)
+# app.include_router(analytics.router)  # will be uncomment once backend.routes.analytics exists
 
 @app.get("/")
 def root():
